3cf8dbac287f57edd778f9e9d963222e5b3c4273
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-30 19:56:29 +0530

    add remove duplicates from array

---
+++ removeDuplicate.js
@@ -0,0 +1,15 @@
+//26. Remove Duplicates from Sorted Array
+
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i + 1, 1);
+      i--;
+    }
+  }
+  return nums;
+};
+
+const result = removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+
+console.log(result);
