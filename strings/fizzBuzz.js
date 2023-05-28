938ae5fd88a32b0170e04e1cbd9f4d4dca98e813
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-05-28 19:31:10 +0530

    add fizzbuzz solution

---
+++ strings/fizzBuzz.js
@@ -0,0 +1,20 @@
+//412. Fizz Buzz
+
+var fizzBuzz = function (n) {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(String(i));
+    }
+  }
+  return result;
+};
+
+const result = fizzBuzz(5);
+console.log(result);
