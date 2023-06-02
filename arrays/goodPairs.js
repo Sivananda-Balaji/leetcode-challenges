96ac286d172c824764492f7d62b990032934d1b6
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-06-02 08:27:51 +0530

    add goodpairs solution

---
+++ arrays/goodPairs.js
@@ -0,0 +1,17 @@
+//1512. Number of Good Pairs
+
+var numIdenticalPairs = function (nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j] && i < j) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+const result = numIdenticalPairs([1, 1, 1, 1]);
+
+console.log(result);
