37181956b0891644b6ec85c429a909884c45f258
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-07-07 12:33:10 +0530

    add move zeros solution

---
+++ leetcode_75/moveZeros.js
@@ -0,0 +1,18 @@
+//283. Move Zeroes
+
+var moveZeroes = function (nums) {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index++] = nums[i];
+    }
+  }
+  for (let i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
+};
+
+const result = moveZeroes([0, 1, 0, 3, 12]);
+
+console.log(result);
