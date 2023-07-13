47322e3c4d721e1363258302812ee2b73df8e915
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-07-13 11:46:42 +0530

    add group anagrams solution

---
+++ top_interview_150/group-anagrams.js
@@ -0,0 +1,20 @@
+//49. Group Anagrams
+
+var groupAnagrams = function (strs) {
+  const map = {};
+  for (let str of strs) {
+    const sortedStr = str.split("").sort().join("");
+    if (map[sortedStr]) {
+      map[sortedStr].push(str);
+    } else {
+      map[sortedStr] = [str];
+    }
+  }
+  return Object.values(map);
+};
+
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+const result = groupAnagrams(strs);
+
+console.log(result);
