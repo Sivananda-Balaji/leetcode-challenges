4ffcad6aa8f6e8dc1accbb82c0ee2c2856057c7d
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-07-15 14:10:06 +0530

    add first and last position of element in a sorted array solution

---
+++ top_interview_150/first-last-position.js
@@ -0,0 +1,41 @@
+//34. Find First and Last Position of Element in Sorted Array
+
+var searchRange = function (nums, target) {
+  const answer = [-1, -1];
+  let start = 0,
+    end = nums.length - 1;
+  if (nums.length === 1) {
+    return nums[start] === target ? [start, end] : [-1, -1];
+  }
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      answer[0] = mid;
+      end = mid - 1;
+    } else if (nums[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  (start = 0), (end = nums.length - 1);
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      answer[1] = mid;
+      start = mid + 1;
+    } else if (nums[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return answer;
+};
+
+const nums = [5, 7, 7, 8, 8, 10];
+target = 8;
+
+const result = searchRange(nums, target);
+
+console.log(result);
