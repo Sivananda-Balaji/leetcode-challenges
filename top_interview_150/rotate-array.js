d815daef5db86664dd759b37564c16b899f96d53
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-08-05 16:25:16 +0530

    update rotate array solution

--- top_interview_150/rotate-array.js
+++ top_interview_150/rotate-array.js
@@ -1,14 +1,30 @@
 //189. Rotate Array
 
 var rotate = function (nums, k) {
+  k = k % nums.length;
+  const reverseArr = (start, end) => {
+    while (start < end) {
+      [nums[start], nums[end]] = [nums[end], nums[start]];
+      start++;
+      end--;
+    }
+  };
+  reverseArr(0, nums.length - 1);
+  reverseArr(0, k - 1);
+  reverseArr(k, nums.length - 1);
+};
+
+const nums = [-1];
+k = 2;
+
+rotate(nums, k);
+
+console.log(nums);
+
+/**
+ * var rotate = function (nums, k) {
   k %= nums.length;
   nums.unshift(...nums.splice(-k));
   return nums;
 };
-
-const nums = [1, 2],
-  k = 3;
-
-const result = rotate(nums, k);
-
-console.log(result);
+ */
