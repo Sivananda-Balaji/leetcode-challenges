f187bcb626a74119f144617cd2bbb67ad462d0a2
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-11-07 11:04:02 +0530

    add is-subsequence solution

---
+++ revision/is-subsequence.js
@@ -0,0 +1,18 @@
+//392. Is Subsequence
+
+var isSubsequence = function (s, t) {
+  let count = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (s[count] === t[i]) {
+      count++;
+    }
+  }
+  return count === s.length;
+};
+
+const s = "abc",
+  t = "ahbgdc";
+
+const result = isSubsequence(s, t);
+
+console.log(result);
