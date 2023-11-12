63a15a13375f81dfed9985828c79ce6eb6227acc
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-11-12 09:31:06 +0530

    update two sum solution with map

--- revision/two-sum.js
+++ revision/two-sum.js
@@ -1,14 +1,13 @@
 //1. Two Sum
 
 var twoSum = function (nums, target) {
-  const map = {};
+  const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    const num = target - nums[i];
-    if (map[num] !== undefined) {
-      return [map[num], i];
-    } else {
-      map[nums[i]] = i;
+    const newNum = target - nums[i];
+    if (map.has(newNum)) {
+      return [map.get(newNum), i];
     }
+    map.set(nums[i], i);
   }
 };
 
