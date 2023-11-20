5bc246c3029fb8f231e75580b0a0ff599a0f3468
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-11-20 09:58:39 +0530

    update reverse words solution

--- revision/reverse-words.js
+++ revision/reverse-words.js
@@ -1,18 +1,8 @@
 //151. Reverse Words in a String
 
 var reverseWords = function (s) {
-  s = s.trim();
-  let answer = "";
-  let temp = "";
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] === " " && temp.length > 0) {
-      answer += temp + " ";
-      temp = "";
-    } else if (s[i] !== " ") {
-      temp = s[i] + temp;
-    }
-  }
-  return (answer + temp).trim();
+  const wordsArr = s.trim().split(/\s+/);
+  return wordsArr.reverse().join(" ");
 };
 
 const s = "a good   example";
