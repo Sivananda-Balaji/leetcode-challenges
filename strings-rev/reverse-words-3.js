9b999ad05b761ed14104d5e5b23f8153f9dc3f41
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2024-02-18 18:05:53 +0530

    add reverse words in a string III solution

---
+++ strings-rev/reverse-words-3.js
@@ -0,0 +1,22 @@
+//557. Reverse Words in a String III
+
+var reverseWords = function (s) {
+  let answer = "";
+  let words = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== " ") {
+      words = s[i] + words;
+    } else {
+      answer += words + " ";
+      words = "";
+    }
+  }
+  answer += words;
+  return answer;
+};
+
+const s = "Let's take LeetCode contest";
+
+const result = reverseWords(s);
+
+console.log(result);
