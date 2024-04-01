40faf21814d33a24d8fc4b942135c822eaf5e565
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2024-04-01 06:46:43 +0530

    add length of last word solution

---
+++ april_2024/last-word.js
@@ -0,0 +1,18 @@
+//58. Length of Last Word
+
+var lengthOfLastWord = function (s) {
+  s = s.trim();
+  let answer = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " ") {
+      return answer.length;
+    }
+    answer = s[i] + answer;
+  }
+};
+
+const s = "Hello World";
+
+const result = lengthOfLastWord(s);
+
+console.log(result);
