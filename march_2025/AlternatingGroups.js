be476ffe612041e6b318049999fa03511c4e2357
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-03-11 19:38:15 +0530

    intial setup for code

--- march_2025/AlternatingGroups.js
+++ march_2025/AlternatingGroups.js
@@ -1,24 +1,6 @@
 // 3208. Alternating Groups II
 
-var numberOfAlternatingGroups = function (colors, k) {
-  const n = colors.length;
-  const extendedColors = [...colors, ...colors.slice(0, k - 1)]; // Extend the array
-
-  let count = 0;
-
-  for (let i = 0; i < n; i++) {
-    let isValid = true;
-    for (let j = i; j < i + k - 1; j++) {
-      if (extendedColors[j] === extendedColors[j + 1]) {
-        isValid = false;
-        break;
-      }
-    }
-    if (isValid) count++;
-  }
-
-  return count;
-};
+var numberOfAlternatingGroups = function (colors, k) {};
 
 const colors = [1, 1, 0, 1];
 k = 4;
