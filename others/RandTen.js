d2ac3f87be6991709e1276c0afd036caf99dcb8e
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-04-17 22:30:05 +0530

    update random10 using random7 solution

--- others/RandTen.js
+++ others/RandTen.js
@@ -1,13 +1,13 @@
 //470. Implement Rand10() Using Rand7()
 
 var rand10 = function () {
-  let a, b, val;
-  do {
-    a = Math.floor(Math.random() * 7) + 1;
-    b = Math.floor(Math.random() * 7) + 1;
-    val = (a - 1) * 7 + b;
-  } while (val > 40);
-  return 1 + ((val - 1) % 10);
+  const a = Math.floor(Math.random() * 7) + 1;
+  const b = Math.floor(Math.random() * 7) + 1;
+  const val = (a - 1) * 7 + b;
+  if (val > 40) {
+    return rand10();
+  }
+  return (val % 10) + 1;
 };
 
 const n = 2;
