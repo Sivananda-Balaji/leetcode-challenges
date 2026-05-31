0f11d1b837b1d5045720514d15309d3cf83d8270
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2026-05-31 09:19:48 +0530

    implement merge sort for sorting algorithm

--- 2026/may_2026/AsteroidsDestroyed.js
+++ 2026/may_2026/AsteroidsDestroyed.js
@@ -1,7 +1,41 @@
 //2126. Destroying Asteroids
 
+const merge = (left, right) => {
+  const result = [];
+  let i = 0,
+    j = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+  while (i < left.length) {
+    result.push(left[i]);
+    i++;
+  }
+  while (j < right.length) {
+    result.push(right[j]);
+    j++;
+  }
+  return result;
+};
+
+const mergeSort = (asteroids) => {
+  if (asteroids.length <= 1) {
+    return asteroids;
+  }
+  const mid = Math.floor(asteroids.length / 2);
+  const left = mergeSort(asteroids.slice(0, mid));
+  const right = mergeSort(asteroids.slice(mid));
+  return merge(left, right);
+};
+
 var asteroidsDestroyed = function (mass, asteroids) {
-  asteroids.sort((a, b) => a - b);
+  asteroids = mergeSort(asteroids);
   for (let i = 0; i < asteroids.length; i++) {
     if (mass < asteroids[i]) {
       return false;
