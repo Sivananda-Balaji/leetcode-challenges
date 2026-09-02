34927d947fad24347fc964677190ec60b30409cc
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2026-09-02 07:52:50 +0530

    add uniform parity array solution to the project

---
+++ 2026/september_2026/UniformArray.js
@@ -0,0 +1,11 @@
+//3875. Construct Uniform Parity Array I
+
+var uniformArray = function (nums1) {
+  return true;
+};
+
+const nums1 = [2, 3];
+
+const result = uniformArray(nums1);
+
+console.log(result);
